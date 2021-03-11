049891b13986bda2229c500829460029ccd839df
Cauezera <71109768+Cauezera@users.noreply.github.com>
2021-03-11 19:08:30 -0300

    Add files via upload

--- todo/main.js
+++ todo/main.js
@@ -1,5 +1,25 @@
 const buttonAddTask = document.querySelector("#button-add-task")
+const buttonEdit = document.querySelector("#btn-check-edit")
+const inputTask = document.querySelector("#input-task")
+const alertAlter = document.querySelector("#alert-alter")
+
 
 buttonAddTask.addEventListener("click", () => {
     // const response = await fetch 
 })
+
+let bool = 2;
+
+buttonEdit.addEventListener("click", () => {
+    if (bool%2 == 0) {
+        inputTask.removeAttribute("disabled")
+        bool++
+    } else {
+        inputTask.setAttribute("disabled", "disabled")
+        bool--
+        alertAlter.classList.remove("d-none")
+        setTimeout(function(){alertAlter.classList.add("d-none")}, 2000 )
+    }
+})  
+
+        
